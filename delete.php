b761c1fe83c931783b41eee50938b5150916b50b
SaikouTrawally <B00161930@mytudublin.ie>
2025-11-25 15:31:50 +0000

    Added delete.php to remove records from database

---
+++ delete.php
@@ -0,0 +1,12 @@
+<?php
+include 'database.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "DELETE FROM gym_logs WHERE id = $id";
+    $conn->query($sql);
+}
+
+header("Location: display.php");
+exit;
+?>
