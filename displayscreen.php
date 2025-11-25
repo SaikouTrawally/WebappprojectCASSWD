e318459b26d3e109b6fbe2e85d88a1b437920a3d
SaikouTrawally <B00161930@mytudublin.ie>
2025-11-25 15:07:16 +0000

    Create display page to show all workouts

---
+++ displayscreen.php
@@ -0,0 +1,43 @@
+<?php  
+include "database.php";
+
+$result = mysqli_query($conn, "SELECT * FROM workouts ORDER BY date_logged DESC");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>All Workouts</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1>Workout History</h1>
+
+<table>
+    <tr>
+        <th>Type</th>
+        <th>Exercise</th>
+        <th>Sets</th>
+        <th>Reps</th>
+        <th>Weight</th>
+        <th>Date</th>
+    </tr>
+
+    <?php while($row = mysqli_fetch_assoc($result)) { ?>
+        <tr>
+            <td><?= $row['type'] ?></td>
+            <td><?= $row['exercise'] ?></td>
+            <td><?= $row['sets'] ?></td>
+            <td><?= $row['reps'] ?></td>
+            <td><?= $row['weight'] ?></td>
+            <td><?= $row['date_logged'] ?></td>
+        </tr>
+    <?php } ?>
+
+</table>
+
+<a href="index.html">Back</a>
+
+</body>
+</html>
