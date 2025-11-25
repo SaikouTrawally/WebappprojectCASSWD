987dd3d3b9283076ca30e5dc8c78ad66f0603d97
SaikouTrawally <B00161930@mytudublin.ie>
2025-11-25 15:02:00 +0000

    Create insert.php to insert workouts into MySQL

---
+++ insertscreen.php
@@ -0,0 +1,21 @@
+<?php  
+include "db.php";
+
+$type = $_POST['type'];
+$exercise = $_POST['exercise'];
+$sets = $_POST['sets'];
+$reps = $_POST['reps'];
+$weight = $_POST['weight'];
+
+$sql = "INSERT INTO workouts (type, exercise, sets, reps, weight)
+        VALUES ('$type', '$exercise', $sets, $reps, $weight)";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Workout added! <br><br>";
+    echo "<a href='index.html'>Back</a>";
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
