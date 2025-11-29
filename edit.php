5e23c80c510acd148642395c6158cce0f64e1a7d
SaikouTrawally <B00161930@mytudublin.ie>
2025-11-29 13:25:10 +0000

    Replaced exercise text input with dropdown menu

--- edit.php
+++ edit.php
@@ -7,15 +7,8 @@ if (!isset($_GET['id'])) {
 }
 
 $id = $_GET['id'];
-
 $sql = "SELECT * FROM gym_logs WHERE id = $id";
 $result = $conn->query($sql);
-
-if ($result->num_rows == 0) {
-    echo "Record not found.";
-    exit;
-}
-
 $row = $result->fetch_assoc();
 ?>
 
@@ -23,25 +16,33 @@ $row = $result->fetch_assoc();
 <html>
 <head>
     <title>Edit Entry</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
 <h2>Edit Workout Entry</h2>
 
 <form action="update.php" method="POST">
+
     <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
 
     <label>Exercise:</label>
-    <input type="text" name="exercise" value="<?php echo $row['exercise']; ?>" required><br><br>
+    <select name="exercise" required>
+        <option value="Bench Press" <?php if ($row['exercise']=="Bench Press") echo "selected"; ?>>Bench Press</option>
+        <option value="Squat" <?php if ($row['exercise']=="Squat") echo "selected"; ?>>Squat</option>
+        <option value="Deadlift" <?php if ($row['exercise']=="Deadlift") echo "selected"; ?>>Deadlift</option>
+        <option value="Shoulder Press" <?php if ($row['exercise']=="Shoulder Press") echo "selected"; ?>>Shoulder Press</option>
+    </select>
 
     <label>Weight (kg):</label>
-    <input type="number" name="weight" value="<?php echo $row['weight']; ?>" required><br><br>
+    <input type="number" name="weight" value="<?php echo $row['weight']; ?>" required>
 
     <label>Reps:</label>
-    <input type="number" name="reps" value="<?php echo $row['reps']; ?>" required><br><br>
+    <input type="number" name="reps" value="<?php echo $row['reps']; ?>" required>
 
     <button type="submit">Update</button>
 </form>
 
 </body>
 </html>
+
