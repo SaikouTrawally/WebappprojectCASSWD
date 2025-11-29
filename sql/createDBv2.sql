e943bdd8ae670d3389fcaf4276756a1906a67bc1
SaikouTrawally <B00161930@mytudublin.ie>
2025-11-29 13:31:18 +0000

    Added table for exerise tables and CreateDBv2.sql

---
+++ sql/createDBv2.sql
@@ -0,0 +1,7 @@
+CREATE TABLE exercises (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO exercises (name) VALUES
+('Bench Press'), ('Squat'), ('Deadlift'), ('Shoulder Press');
